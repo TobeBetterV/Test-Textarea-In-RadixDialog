2dceb67fa0e1c0a4906059fdef146cd3e21fbefa
guoaoli <tobe_better@outlook.com>
2024-10-24 10:58:00 +0800

    Initial commit

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,15 @@
+// async function getProjects(){
+//     const res =await fetch(`https://...`,{catch:'no-store'})
+//     const projects = await res.json()
+//
+//     return projects
+// }
+
+export default async function Dashboard(){
+    // const projects = await getProjects()
+    return (
+        <ul>
+            Test
+        </ul>
+    )
+}
